c0e126a7656e78bcb61f6f57909e6ee369ddddf3
shryzhang <996707719@qq.com>
2019-08-27 17:57:55 +0800

    DEV:尝试去除element-ui打包

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css' // 引入element-ui样式文件，否则样式不生效
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css' // 引入element-ui样式文件，否则样式不生效
 
-Vue.use(ElementUI)
+// Vue.use(ElementUI) // externals掉element-ui，由用户环境提供
 
 new Vue({
 	el: '#app',
